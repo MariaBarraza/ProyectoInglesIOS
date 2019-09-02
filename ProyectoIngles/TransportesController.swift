9cbe6d9a75b44b7cb65cbe9f101f469e7e72878e
Alumno <ulsa@iMacD105.local>
2019-09-02 15:00:33 -0700

    Controller y contraints en proceso

--- ProyectoIngles/TransportesController.swift
+++ ProyectoIngles/TransportesController.swift
@@ -7,3 +7,43 @@
 //
 
 import Foundation
+import UIKit
+
+import AVFoundation
+
+class TransportesController : UIViewController{
+    
+    //MUSICA FONDO
+    var player : AVAudioPlayer?
+    var playerMusicaFondo : AVAudioPlayer?
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let path = Bundle.main.path(forResource: "AUDIO 29.wav", ofType: nil)
+        let url = URL(fileURLWithPath: path!)
+        
+        
+        
+        do {
+            
+            player = try AVAudioPlayer(contentsOf: url)
+            let urlMusicaFondo = URL(fileURLWithPath: pathMusicaFondo!)
+            playerMusicaFondo?.play()
+            
+        } catch {
+            
+            
+            
+        }
+        
+    }
+    
+    
+    
+}
+
+
