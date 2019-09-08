9d0a4c1140556f0b3c8f31678018757426d329a0
Ana Lucia Blanco <analuciablanco@outlook.com>
2019-09-08 16:18:47 -0700

    Solo falta añadir las animaciones correctas

--- ProyectoIngles/FrutasController.swift
+++ ProyectoIngles/FrutasController.swift
@@ -53,16 +53,51 @@ class FrutasController: UIViewController {
         var arregloFruta : [String] = []
         var arregloUIImage : [UIImage] = []
         
-        let arregloManzana : [String] = ["animApple00", "animApple01", "animApple02", "animApple03", "animApple04", "animApple05", "animApple06", "animApple07", "animApple08", "animApple09", "animApple10", "animApple11", "animApple12", "animApple13", "animApple14", "animApple15", "animApple16", "animApple17", "animApple18", "animApple19", "animApple20", "animApple21", "animApple22", "animApple23", "animApple24"]
-        let arregloFresa : [String] = ["animFresaDurmiendo00", "animFresaDurmiendo01", "animFresaDurmiendo02", "animFresaDurmiendo03", "animFresaDurmiendo04", "animFresaDurmiendo05", "animFresaDurmiendo06", "animFresaDurmiendo07", "animFresaDurmiendo08", "animFresaDurmiendo09", "animFresaDurmiendo10", "animFresaDurmiendo11", "animFresaDurmiendo12", "animFresaDurmiendo13", "animFresaDurmiendo14", "animFresaDurmiendo15"]
-        let arregloCereza : [String] = ["animCerezasMin00", "animCerezasMin01", "animCerezasMin02", "animCerezasMin03", "animCerezasMin04", "animCerezasMin05", "animCerezasMin06", "animCerezasMin07", "animCerezasMin08", "animCerezasMin09", "animCerezasMin10", "animCerezasMin11", "animCerezasMin12", "animCerezasMin13", "animCerezasMin14", "animCerezasMin15", "animCerezasMin16", "animCerezasMin17", "animCerezasMin18", "animCerezasMin19"]
-        let arregloKiwi : [String] = ["animKiwi00", "animKiwi01", "animKiwi02", "animKiwi03", "animKiwi04", "animKiwi05", "animKiwi06", "animKiwi07", "animKiwi08", "animKiwi09", "animKiwi10", "animKiwi11", "animKiwi12", "animKiwi13", "animKiwi14", "animKiwi15"]
-        let arregloLimon : [String] = ["animLemon00.png", "animLemon01.png", "animLemon02.png", "animLemon03.png", "animLemon04.png", "animLemon05.png", "animLemon06.png", "animLemon07.png", "animLemon08.png", "animLemon09.png", "animLemon10.png", "animLemon11.png", "animLemon12.png", "animLemon13.png", "animLemon14.png", "animLemon15.png", "animLemon16.png", "animLemon17.png", "animLemon18.png", "animLemon19.png"]
-        let arregloPlatano : [String] = ["animBanana00", "animBanana01", "animBanana02", "animBanana03", "animBanana04", "animBanana05", "animBanana06", "animBanana07", "animBanana08", "animBanana09", "animBanana10", "animBanana11", "animBanana12", "animBanana13", "animBanana14", "animBanana15"]
-        let arregloNaranja : [String] = ["animNaranjaMin00", "animNaranjaMin01", "animNaranjaMin02", "animNaranjaMin03", "animNaranjaMin04", "animNaranjaMin05", "animNaranjaMin06", "animNaranjaMin07", "animNaranjaMin08", "animNaranjaMin09", "animNaranjaMin10", "animNaranjaMin11", "animNaranjaMin12", "animNaranjaMin13", "animNaranjaMin14", "animNaranjaMin15", "animNaranjaMin16", "animNaranjaMin17", "animNaranjaMin18", "animNaranjaMin19", "animNaranjaMin20", "animNaranjaMin21", "animNaranjaMin22", "animNaranjaMin23", "animNaranjaMin24", "animNaranjaMin25", "animNaranjaMin26", "animNaranjaMin27", "animNaranjaMin28", "animNaranjaMin29", "animNaranjaMin30", "animNaranjaMin31"]
-        let arregloPera : [String] = ["animPera00", "animPera01", "animPera02", "animPera03", "animPera04", "animPera05", "animPera06", "animPera07", "animPera08", "animPera09", "animPera10", "animPera11", "animPera12", "animPera13", "animPera14", "animPera15", "animPera16", "animPera17", "animPera18", "animPera19", "animPera20", "animPera21", "animPera22", "animPera23", "animPera24", "animPera25", "animPera26", "animPera27", "animPera28", "animPera29"]
-        let arregloSandia : [String] = ["animSandiamin00", "animSandiamin01", "animSandiamin02", "animSandiamin03", "animSandiamin04", "animSandiamin05", "animSandiamin06", "animSandiamin07", "animSandiamin08", "animSandiamin09", "animSandiamin10", "animSandiamin11", "animSandiamin12", "animSandiamin13", "animSandiamin14"]
-        
+        // Manzana
+        var arregloManzana : [String] = []
+        for i in 0...24{
+            arregloManzana.append("animApple" + "\(i)")
+        }
+        // Fresa
+        var arregloFresa : [String] = []
+        for i in 0...15{
+            arregloFresa.append("animFresaDurmiendo" + "\(i)")
+        }
+        // Cereza
+        var arregloCereza : [String] = []
+        for i in 0...19{
+            arregloCereza.append("animCerezasMin" + "\(i)")
+        }
+        // Kiwi
+        var arregloKiwi : [String] = []
+        for i in 0...15{
+            arregloKiwi.append("animKiwi" + "\(i)")
+        }
+        // Limon
+        var arregloLimon : [String] = []
+        for i in 0...19{
+            arregloLimon.append("animLemon" + "\(i)")
+        }
+        // Platano
+        var arregloPlatano : [String] = []
+        for i in 0...15{
+            arregloPlatano.append("animBanana" + "\(i)")
+        }
+        // Naranja
+        var arregloNaranja : [String] = []
+        for i in 0...31{
+            arregloNaranja.append("animNaranjaMin" + "\(i)")
+        }
+        // Pera
+        var arregloPera : [String] = []
+        for i in 0...29{
+            arregloPera.append("animPera" + "\(i)")
+        }
+        // Sandia
+        var arregloSandia : [String] = []
+        for i in 0...14{
+            arregloSandia.append("animSandiamin" + "\(i)")
+        }
         
         switch nombreFruta {
         case "Manzana":
